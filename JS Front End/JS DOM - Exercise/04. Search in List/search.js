0dd7fa2eecb1bfc202e90427ca18681764dcfbc7
Aleksandar Kostadinov <110054955+ProXy97@users.noreply.github.com>
2023-12-10 21:45:52 +0000

    Updated

--- JS Front End/JS DOM - Exercise/04. Search in List/search.js
+++ JS Front End/JS DOM - Exercise/04. Search in List/search.js
@@ -1,3 +1,27 @@
 function search() {
-   // TODO
+   let list = Array.from(document.querySelectorAll("li"));
+   let searching = document.getElementById("searchText").value;
+   let result = document.getElementById("result");
+   let matches = 0;
+
+   for (const town of list) 
+   {
+      
+         town.style.textDecoration = '';
+         town.style.fontWeight = '';
+
+   }
+
+   for (const town of list) 
+   {
+      if(town.textContent.includes(searching))
+      {
+         matches++;
+         town.style.textDecoration = "underline";
+         town.style.fontWeight = "bold";
+      }
+   }
+
+   result.textContent = `${matches} matches found`;
+   
 }
--- JS Front End/JS DOM - Exercise/05. Table - Search Engine/solution.js
+++ JS Front End/JS DOM - Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,23 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
+   let searchingText = document.getElementById("searchField");
+   let tableRows = document.querySelectorAll("tbody tr");
+
+
    function onClick() {
-      //   TODO:
+      
+      for (const row of tableRows) {
+         row.className = '';
+      }
+
+      for (const row of tableRows) {
+         if(searchingText.value !== '' && row.innerHTML.includes(searchingText.value))
+         {
+            row.className = 'select';
+         }
+      }
+      searchingText.value = '';
 
    }
 }
